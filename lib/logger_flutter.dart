33d08ef0122d905543806ae13e19e49b0658c347
Peter Stojanowski <peter.stojanowski.external@xayn.com>
2021-12-15 15:24:08 +0000

    Flutter 2.8.0 upgrade

--- lib/logger_flutter.dart
+++ lib/logger_flutter.dart
@@ -3,11 +3,8 @@ library logger_flutter;
 
 import 'dart:async';
 import 'dart:collection';
-import 'dart:ui';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:logger/logger.dart';
 
 part 'src/log_console.dart';
